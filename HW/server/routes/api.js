8efcb3d91fb08291bfeef909e5785fe9abc6532c
Serhii Kolisnychenko <49130sk@gmail.com>
2020-01-28 16:28:39 +0200

    refactoring

--- HW/server/routes/api.js
+++ HW/server/routes/api.js
@@ -1,23 +1,7 @@
 const log = require(INCPATH + '/log')(module);
 const express = require('express');
 const router = express.Router();
-const path = require('path'); //helps to establish the right ways
 const fs = require("fs");
-// const UserModel = require(INCPATH + '/mongoose').UserModel;
-
-// router.get('/some-request', function (req, res) {
-//   const user = UserModel({
-//     name: 'test'
-//   });
-
-//   UserModel.find((err, users) => {
-//     if (err) {
-//       log.error('Error find users in Mongo');
-//     }
-//     log.info('Users finds');
-//     res.end(JSON.stringify(users));
-//   });
-// });
 
 let list;
 fs.readFile("./config/articles.json", "utf8", function(err, data) {
