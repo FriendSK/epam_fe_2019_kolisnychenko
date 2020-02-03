9c930752e08cd25b1ef7027e3dfd4eff263a1447
Serhii Kolisnychenko <49130sk@gmail.com>
2020-02-03 02:35:56 +0200

    HW26-Patterns

--- HW/js/mediator.js
+++ HW/js/mediator.js
@@ -1,39 +1,92 @@
 import '../scss/main4.scss';
 import articles from './articles';
 
-const leftMenu = document.querySelector('.left-menu__authors ul');
-const rightMenu = document.querySelector('.right-menu__authors ul');
+const mediator = (function () {
+  const subscribers = {};
 
-const mediator = ( function () {
-const subscribers = {};
-
-return {
-    subscribe: function (event, callback) {
-        subscribers[event] = subscribers[event] || [];
-        subscribers[event].push(callback);
+  return {
+    subscribe: (event, callback) => {
+      subscribers[event] = subscribers[event] || [];
+      subscribers[event].push(callback);
     },
 
-    unsubscribe: function (event, callback) {
-        let subscriberIndex;
+    unsubscribe: (event, callback) => {
+      let subscriberIndex;
 
-        if (!event) {
-            subscribers = {};
-        } else if (event && !callback) {
-            subscribers[event] = [];
-        } else {
-            subscriberIndex = subscribers[event].indexOf(callback);
-            if (subscriberIndex > -1) {
-                subscribers[event].splice(subscriberIndex, 1);
-            }
+      if (!event) {
+        subscribers = {};
+      } else if (event && !callback) {
+        subscribers[event] = [];
+      } else {
+        subscriberIndex = subscribers[event].indexOf(callback);
+        if (subscriberIndex > -1) {
+          subscribers[event].splice(subscriberIndex, 1);
         }
+      }
     },
 
-    publish: function (event, data) {
+    publish: function (event, data, e) {
       if (subscribers[event]) {
-        subscribers[event].forEach(function (callback) {
-          callback(data);
+        subscribers[event].forEach((callback) => {
+          callback(data, e);
         });
       }
     },
   };
 })();
+class Menu {
+  constructor(articles, side) {
+    this.side = side;
+    this.articles = articles;
+  }
+
+  render() {
+    const ul = document.createElement('ul');
+    ul.className = 'menu__authors';
+    articles.map((article) => {
+      const menuItem = document.createElement('li');
+      menuItem.className = 'menu__author';
+      menuItem.dataset.name = (`${article.author}`)
+      menuItem.innerHTML = `${article.author}`;
+      ul.appendChild(menuItem);
+    });
+    document.querySelector(this.side).appendChild(ul);
+  };
+}
+
+class TopMenu extends Menu {
+  showTopPosts(articles, e) {
+    const posts = document.querySelectorAll('.menu__posts');
+    const index = articles.findIndex((article) => article.author === e.target.dataset.name);
+    const menuItem = document.createElement('span');
+    menuItem.className = 'menu__posts';
+    menuItem.innerHTML = `${articles[index].title}`;
+    document.querySelectorAll('.top-menu .menu__author')[index].appendChild(menuItem);
+    posts.forEach((elem) => {
+      elem.remove();
+    });
+  }
+}
+
+class AsideMenu extends Menu {
+  showAsidePosts(articles, e) {
+    const index = articles.findIndex((article) => article.author === e.target.dataset.name);
+    const menuItem = document.createElement('span');
+    menuItem.className = 'menu__posts';
+    menuItem.innerHTML = `${articles[index].title}`;
+    document.querySelectorAll('.aside-menu .menu__author')[index].appendChild(menuItem);
+  }
+}
+
+const createMenu = (articles) => {
+  const topMenu = new TopMenu(articles, '.top-menu');
+  topMenu.render();
+  const asideMenu = new AsideMenu(articles, '.aside-menu');
+  asideMenu.render();
+  const menuItems = document.querySelectorAll('.menu__author');
+  menuItems.forEach((item) => item.addEventListener('click', (e) => mediator.publish('clickOnAuthor', articles, e)));
+  mediator.subscribe('clickOnAuthor', topMenu.showTopPosts);
+  mediator.subscribe('clickOnAuthor', asideMenu.showAsidePosts);
+}
+
+createMenu(articles);
